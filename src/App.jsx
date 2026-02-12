b42a433ee2fbdbca0dc392951212ab2016e8d68a
leviferre1ra <leviferreira1677@gmail.com>
2026-02-12 12:03:06 -0300

    feat: initialize project with React and Vite setup
    - Added main application structure with App component
    - Implemented FormularioDeEvento for event creation
    - Styled application with App.css
    - Included logo and banner images
    - Added SVG icons for React and Vite
    - Configured Vite for React support

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+// no react, componentes são FUNÇÕES
+
+function FormularioDeEvento() {
+  
+  return (
+    <form className='form-evento'> 
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="nome">
+          Qual o nome do evento?
+        </label>
+        <input type="text" id='nome'/>
+      </fieldset>
+    </form>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/logoTecboard.png" alt="" />
+      </header>
+      <section>
+        <img src="/bannerTecboard.png" alt="" />
+      </section>
+      <FormularioDeEvento/>
+    </main>
+  )
+}
+
+export default App
